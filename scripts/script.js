a67d5b7ffe11f0b8e0f4d5dc728e0e37b328aa83
KELLSPING\kells <kellsping@gmail.com>
2023-02-10 21:30:41 +0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -33,19 +33,19 @@ function changeTheme() {
 let redScore = 0;
 let mRedScore = document.getElementById("red-score");
 
-function redScorePlusOne(){
+function redScorePlusOne() {
     redScore += 1;
     mRedScore.textContent = redScore;
 }
-function redScorePlusTwo(){
+function redScorePlusTwo() {
     redScore += 2;
     mRedScore.textContent = redScore;
 }
-function redScorePlusThree(){
+function redScorePlusThree() {
     redScore += 3;
     mRedScore.textContent = redScore;
 }
-function redScoreClear(){
+function redScoreClear() {
     redScore = 0;
     mRedScore.textContent = redScore;
 }
@@ -54,11 +54,11 @@ function redScoreClear(){
 let redFoul = 0;
 let mRedFoul = document.getElementById("red-foul");
 
-function redFoulPlusOne(){
+function redFoulPlusOne() {
     redFoul += 1;
     mRedFoul.textContent = redFoul;
 }
-function redFoulClear(){
+function redFoulClear() {
     redFoul = 0;
     mRedFoul.textContent = redFoul;
 }
@@ -67,19 +67,19 @@ function redFoulClear(){
 let blueScore = 0;
 let mBlueScore = document.getElementById("blue-score");
 
-function blueScorePlusOne(){
+function blueScorePlusOne() {
     blueScore += 1;
     mBlueScore.textContent = blueScore;
 }
-function blueScorePlusTwo(){
+function blueScorePlusTwo() {
     blueScore += 2;
     mBlueScore.textContent = blueScore;
 }
-function blueScorePlusThree(){
+function blueScorePlusThree() {
     blueScore += 3;
     mBlueScore.textContent = blueScore;
 }
-function blueScoreClear(){
+function blueScoreClear() {
     blueScore = 0;
     mBlueScore.textContent = blueScore;
 }
@@ -88,11 +88,11 @@ function blueScoreClear(){
 let blueFoul = 0;
 let mBlueFoul = document.getElementById("blue-foul");
 
-function blueFoulPlusOne(){
+function blueFoulPlusOne() {
     blueFoul += 1;
     mBlueFoul.textContent = blueFoul;
 }
-function blueFoulClear(){
+function blueFoulClear() {
     blueFoul = 0;
     mBlueFoul.textContent = blueFoul;
 }
@@ -101,12 +101,69 @@ function blueFoulClear(){
 let roundNum = 0;
 let mRoundNum = document.getElementById("round-num");
 
-function roundNumPlusOne(){
+function roundNumPlusOne() {
     roundNum += 1;
     mRoundNum.textContent = roundNum;
 }
-function roundNumClear(){
+function roundNumClear() {
     roundNum = 0;
     mRoundNum.textContent = roundNum;
 }
 
+// operate clock
+let h = document.getElementById("hour");
+let m = document.getElementById("minute");
+let s = document.getElementById("second");
+
+let mStart = document.getElementById('time-start');
+let mStop = document.getElementById('time-stop');
+let mReset = document.getElementById('time-reset');
+
+//store a reference to the startTimer variable
+var startTimer = null;
+
+mStart.addEventListener('click', function(){
+    //initialize the variable
+    function startInterval(){
+        startTimer = setInterval(function() {
+            timer();
+        }, 1000);
+    }
+    startInterval();
+})
+
+mStop.addEventListener('click', function () {
+    clearInterval(startTimer)
+})
+
+mReset.addEventListener('click', function(){
+    h.value = 0;
+    m.value = 0;
+    s.value = 0;
+    //stop the timer after pressing "reset"
+    stopInterval()
+})
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+    } else if(s.value != 0){
+        s.value--;
+    } else if(m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value--;
+    } else if(h.value != 0 && m.value == 0){
+        s.value = 59;
+        m.value = 59;
+        h.value--;
+    }
+    return;
+}
+
+//stop the function after pressing the reset button, 
+//so the time wont go down when selecting a new time after pressing reset
+function stopInterval() {
+    clearInterval(startTimer);
+}
