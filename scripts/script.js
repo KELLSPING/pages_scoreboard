600727cb7a692f885e475ec30ecc651bc0b90c1b
KELLSPING\kells <kellsping@gmail.com>
2023-02-10 14:40:27 +0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -8,12 +8,10 @@ const github = "assets/github.svg";
 
 const linkedin_white = "assets/linkedin_white.svg";
 const github_white = "assets/github_white.svg";
-const sun_white = "";
 
 const themeIcon = document.getElementById("theme-icon");
 const linkedinIcon = document.getElementById("linkedin-icon");
 const githubIcon = document.getElementById("github-icon");
-
 
 // Swaps the stylesheet to achieve dark mode.
 function changeTheme() {
@@ -30,3 +28,85 @@ function changeTheme() {
         githubIcon.setAttribute("src", github);
     }
 }
+
+// operate red score
+let redScore = 0;
+let mRedScore = document.getElementById("red-score");
+
+function redScorePlusOne(){
+    redScore += 1;
+    mRedScore.textContent = redScore;
+}
+function redScorePlusTwo(){
+    redScore += 2;
+    mRedScore.textContent = redScore;
+}
+function redScorePlusThree(){
+    redScore += 3;
+    mRedScore.textContent = redScore;
+}
+function redScoreClear(){
+    redScore = 0;
+    mRedScore.textContent = redScore;
+}
+
+// operate red foul
+let redFoul = 0;
+let mRedFoul = document.getElementById("red-foul");
+
+function redFoulPlusOne(){
+    redFoul += 1;
+    mRedFoul.textContent = redFoul;
+}
+function redFoulClear(){
+    redFoul = 0;
+    mRedFoul.textContent = redFoul;
+}
+
+// operate blue score
+let blueScore = 0;
+let mBlueScore = document.getElementById("blue-score");
+
+function blueScorePlusOne(){
+    blueScore += 1;
+    mBlueScore.textContent = blueScore;
+}
+function blueScorePlusTwo(){
+    blueScore += 2;
+    mBlueScore.textContent = blueScore;
+}
+function blueScorePlusThree(){
+    blueScore += 3;
+    mBlueScore.textContent = blueScore;
+}
+function blueScoreClear(){
+    blueScore = 0;
+    mBlueScore.textContent = blueScore;
+}
+
+// operate blue foul
+let blueFoul = 0;
+let mBlueFoul = document.getElementById("blue-foul");
+
+function blueFoulPlusOne(){
+    blueFoul += 1;
+    mBlueFoul.textContent = blueFoul;
+}
+function blueFoulClear(){
+    blueFoul = 0;
+    mBlueFoul.textContent = blueFoul;
+}
+
+// operate blue foul
+let roundNum = 0;
+let mRoundNum = document.getElementById("round-num");
+
+function roundNumPlusOne(){
+    roundNum += 1;
+    mRoundNum.textContent = roundNum;
+}
+function roundNumClear(){
+    roundNum = 0;
+    mRoundNum.textContent = roundNum;
+}
+
